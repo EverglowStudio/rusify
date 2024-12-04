0d38b870963115939d892f4de0599ac31907a14b
FrainL <frainl@outlook.com>
2024-12-04 11:15:57 +0800

    feat: change to use feature and add interface

--- rusify-macro/src/lib.rs
+++ rusify-macro/src/lib.rs
@@ -1,15 +1,15 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemEnum, ItemStruct, ItemFn};
+use syn::{parse_macro_input, ItemEnum, ItemStruct, ItemFn, ItemTrait};
 
 #[proc_macro_attribute]
 pub fn rusify_enum(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemEnum);
     let expanded = quote! {
         #[cfg_attr(feature = "uniffi", derive(uniffi::Enum))]
-        #[cfg_attr(target_arch = "wasm32", derive(tsify_next::Tsify), tsify(into_wasm_abi, from_wasm_abi))]
-        #[cfg_attr(target_env = "ohos", napi_derive_ohos::napi)]
+        #[cfg_attr(feature = "wasm32", derive(tsify_next::Tsify), tsify(into_wasm_abi, from_wasm_abi))]
+        #[cfg_attr(feature = "ohos", napi_derive_ohos::napi)]
         #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
         #input
     };
@@ -21,9 +21,19 @@ pub fn rusify_struct(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemStruct);
     let expanded = quote! {
         #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
-        #[cfg_attr(target_arch = "wasm32", derive(tsify_next::Tsify), tsify(into_wasm_abi, from_wasm_abi))]
-        #[cfg_attr(target_env = "ohos", napi_derive_ohos::napi(object))]
+        #[cfg_attr(feature = "wasm32", derive(tsify_next::Tsify), tsify(into_wasm_abi, from_wasm_abi))]
+        #[cfg_attr(feature = "ohos", napi_derive_ohos::napi(object))]
         #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+        #input
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_attribute]
+pub fn rusify_interface(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemTrait);
+    let expanded = quote! {
+        #[cfg_attr(feature = "uniffi", uniffi::export(callback_interface))]
         #input
     };
     TokenStream::from(expanded)
@@ -34,8 +44,18 @@ pub fn rusify_export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
     let expanded = quote! {
         #[cfg_attr(feature = "uniffi", uniffi::export)]
-        #[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen)]
-        #[cfg_attr(target_env = "ohos", napi_derive_ohos::napi)]
+        #[cfg_attr(feature = "wasm32", wasm_bindgen::prelude::wasm_bindgen)]
+        #[cfg_attr(feature = "ohos", napi_derive_ohos::napi)]
+        #input
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_attribute]
+pub fn rusify_export_async(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+    let expanded = quote! {
+        #[cfg_attr(feature = "uniffi", uniffi::export(async_runtime = "tokio"))]
         #input
     };
     TokenStream::from(expanded)
